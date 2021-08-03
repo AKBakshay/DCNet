614d50f55cfe740e40b7d7f0a76cdfd1492c5550
AKBakshay <akbhola.bhola@gmail.com>
2021-08-04 00:09:03 +0900

    [Modify] debugged setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 export PYTHONPATH='.'
 mkdir src/pretrain
 mkdir src/pretrain/model
+mkdir src/output
+mkdir src/output/model_parameters
+mkdir src/output/predictions
 python3 src/model_setup/download_default_files.py
 tar -xvf src/dataset.tar.gz
 rm src/dataset.tar.gz
