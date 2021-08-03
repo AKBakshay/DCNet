40114f0bae8d6c82d2a20fd0be823ac8cb89030c
AKBakshay <akbhola.bhola@gmail.com>
2021-08-03 23:59:43 +0900

    [Add] download default config files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 export PYTHONPATH='.'
+mkdir src/pretrain
+mkdir src/pretrain/model
 python3 src/model_setup/download_default_files.py
+tar -xvf dataset.tar.gz
+rm dataset.tar.gz
--- src/config/download_defaut_config.py
+++ src/config/download_defaut_config.py
@@ -4,11 +4,11 @@ BASE_DIR = Path("src")
 
 # configs corresponds to be provided files in the drive
 config = {
-    "model_weight": "1eBY5Gnif_KexV2MMjAvJDSu_SWFIgZoh",
-    "dataset": "110yZFcc5AlPzTP57GiQaetKZo0p6OUR8",
+    "model_weight": "1FJsxrulia9KYstv5zRcmnlv1NJX6ocbF",
+    "dataset": "1W9A2GoUTxe0-QzASnV-LlpZhfY8E9tZn",
 }
 
 file_path = {
     "model_weight": BASE_DIR / "pretrain/model/model.pt",
-    "dataset": BASE_DIR / "dataset",
+    "dataset": BASE_DIR / "dataset.tar.gz",
 }
